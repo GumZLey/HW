93eb3d2f508214896c5753e35ff4cc1990cc1753
GumZLey <58941219+GumZLey@users.noreply.github.com>
2022-08-16 09:33:28 +0700

    Create hw.c

---
+++ hw.c
@@ -0,0 +1,15 @@
+// Your name
+/*
+#include <stdio.h>
+
+
+int main()
+{
+    printf("Name      : Ananda Kongkoed\n");
+    printf("Nickname  : Ley\n");
+    printf("DOB       : April 25, 2003\n");
+    printf("StudentID : 6513177\n");
+
+    return 0;
+}
+*/
